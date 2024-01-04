eb09ee5fcf54c89772fc9f5c802713504189694c
João Pedro Bessa <joaopbessa1998@gmail.com>
2024-01-04 19:26:02 +0000

    products js file created

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -4,6 +4,13 @@
 
 export const Shop = () => {
     return (
-        <div style={{ paddingTop: '160px'}}>Shop</div>
+        <div style={{ paddingTop: '160px'}}>
+            <div className='shop'>
+                <div className='shopTitle'>
+                    <h1>Shop</h1>
+                </div>
+                <div className='products'></div>
+            </div>
+        </div>
     )
 }
---
+++ src/products.js
@@ -0,0 +1,73 @@
+import image1 from "./images/iphone.png";
+import image2 from "./images/ipad.png";
+import image3 from "./images/watch.jpeg";
+import image4 from "./images/imac.png";
+import image5 from "./images/macbook.png";
+import image6 from "./images/airpods.png";
+import image7 from "./images/display.jpeg";
+import image8 from "./images/tv.jpeg";
+import image9 from "./images/airpodsmax.png";
+import image10 from "./images/homepod.png";
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'iPhone',
+        price: '1499.00€',
+        productImage: image1
+    },
+    {
+        id: 2,
+        productName: 'iPad',
+        price: '899.00€',
+        productImage: image2
+    },
+    {
+        id: 3,
+        productName: 'Apple Watch',
+        price: '499.00€',
+        productImage: image3
+    },
+    {
+        id: 4,
+        productName: 'iMac',
+        price: '2699.00€',
+        productImage: image4
+    },
+    {
+        id: 5,
+        productName: 'MacBook Pro',
+        price: '2299.00€',
+        productImage: image5
+    },
+    {
+        id: 6,
+        productName: 'AirPods Pro',
+        price: '249.00€',
+        productImage: image6
+    },
+    {
+        id: 7,
+        productName: 'Pro Display XDR',
+        price: '4999.00€',
+        productImage: image7
+    },
+    {
+        id: 8,
+        productName: 'Apple TV 4K',
+        price: '129.00€',
+        productImage: image8
+    },
+    {
+        id: 9,
+        productName: 'AirPods Max',
+        price: '549.00€',
+        productImage: image9
+    },
+    {
+        id: 10,
+        productName: 'HomePod Mini',
+        price: '99.00€',
+        productImage: image10
+    }
+]
