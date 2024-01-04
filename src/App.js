937a8ec8c36e76a6e29dac03617f29d129ee2f4e
João Pedro Bessa <joaopbessa1998@gmail.com>
2024-01-04 18:45:11 +0000

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div>APP</div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' />
+          <Route path='/cart' />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
