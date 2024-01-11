69e8d7667614fe660cee3528e5a291eff23e6ed0
João Pedro Bessa <joaopbessa1998@gmail.com>
2024-01-11 12:38:06 +0000

    cart styles done

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './cart.css'
+import { ShopContext } from '../../context/shop-context';
 
 export default function CartItem(props) {
 
     const {id, productName, price, productImage} = props.data;
+    const { cartItems, addToCart, removeFromCart } = useContext(ShopContext);
 
   return (
     <div className='cartItem'>
-        <img src={productImage} />
+        <img src={productImage} alt='' />
         <div className='description'>
             <p>{productName}</p>
             <p>{price}</p>
+            <div className='countHandler'>
+                <button onClick={() => removeFromCart(id)}> - </button>
+                <input value={cartItems[id]} />
+                <button onClick={() => addToCart(id)}> + </button>
+            </div>
         </div>
     </div>
   )
